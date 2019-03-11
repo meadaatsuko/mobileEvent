73143ca94540212bcdbecd775b62f2a7de867d4f
Wei_Z <30371929+meadaatsuko@users.noreply.github.com>
2019-03-11 10:39:43 +0800

    add css & js files

---
+++ index.js
@@ -0,0 +1,16 @@
+function addEvent(element,type,method) {
+	// body...
+	if (element.addEventListener) {
+		element.addEventListener(type,method,false);
+	}else{
+		element["on" + type] = method;
+	}
+}
+addEvent(window,"load",function (event) {
+	// body...
+	var box = document.querySelector("#box");
+	addEvent(window,"orientationchange",function (event) {
+		// body...
+		box.style.transform = "rotate(" + window.orientation + "deg)";
+	});
+});
